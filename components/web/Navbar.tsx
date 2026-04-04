00e4304a71f61f933536101d3a3e63b6fbd4b6c2
stefan22 <stefanosemail@proton.me>
2026-04-04 15:02:33 +0100

    updates shadcn scheme

--- components/web/Navbar.tsx
+++ components/web/Navbar.tsx
@@ -1,37 +1,55 @@
-import Link from "next/link";
-import {buttonVariants} from "@/components/ui/button";
+import Link from 'next/link';
+import { buttonVariants } from '@/components/ui/button';
 
 export const Navbar = () => {
-    return (
-        <nav className="w-full py-5 flex items-center justify-between">
-            <div className="flex items-center gap-8">
-                <Link href="/">
-                    <h1 className="text-3xl font-bold">
-                        Next<span className="text-blue-500">Pro</span>
-                    </h1>
-                </Link>
-                <div className="flex items-center gap-2">
-                    <Link className={buttonVariants({
-                        variant: "ghost",
-                    })} href="/">Home</Link>
+  return (
+    <nav className="w-full py-5 flex items-center justify-between">
+      <div className="flex items-center gap-8">
+        <Link href="/">
+          <h1 className="text-3xl font-bold">
+            Next<span className="text-blue-500">Pro</span>
+          </h1>
+        </Link>
+        <div className="flex items-center gap-2">
+          <Link
+            className={buttonVariants({
+              variant: 'ghost',
+            })}
+            href="/">
+            Home
+          </Link>
 
-                    <Link className={buttonVariants({
-                        variant: "ghost",
-                    })} href="/blog">Blog</Link>
+          <Link
+            className={buttonVariants({
+              variant: 'ghost',
+            })}
+            href="/blog">
+            Blog
+          </Link>
 
-                    <Link className={buttonVariants({
-                        variant: "ghost",
-                    })} href="/create">Create</Link>
-                </div>
-            </div>
-            <div className="flex items-center gap-2">
-                <Link className={buttonVariants()} href="/auth/sign-up">Sign Up</Link>
-                <Link className={buttonVariants({
-                    variant: "outline",
-                })} href="/auth/sign-in">Sign In</Link>
-            </div>
-        </nav>
-    )
-}
+          <Link
+            className={buttonVariants({
+              variant: 'ghost',
+            })}
+            href="/create">
+            Create
+          </Link>
+        </div>
+      </div>
+      <div className="flex items-center gap-2">
+        <Link className={buttonVariants()} href="/auth/sign-up">
+          Sign Up
+        </Link>
+        <Link
+          className={buttonVariants({
+            variant: 'outline',
+          })}
+          href="/auth/sign-in">
+          Sign In
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
