6e8f44d878db5b855ae1c1783205c021ffffb36d
Stefano L <stefanosemail@proton.me>
2026-04-25 16:51:45 +0100

    Signin (#6)
    * GoogleLogo
    
    * signin/signup

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { signInSchema } from '@/app/schemas/auth';
+import Image from 'next/image';
 import { useForm, Controller } from 'react-hook-form';
 import {
   Field,
@@ -26,6 +27,7 @@ import { useRouter } from 'next/navigation';
 import { useTransition } from 'react';
 import { Loader2 } from 'lucide-react';
 import { cn } from '@/lib/utils';
+import GoogleLogo from '@/components/web/GoogleLogo';
 
 export default function SignInPage() {
   const form = useForm({
@@ -61,96 +63,153 @@ export default function SignInPage() {
   };
 
   return (
-    <Card className="px-4 py-8 animate-in fade-in slide-in-from-bottom-4 duration-500">
-      <CardHeader>
-        <CardTitle>Sign In</CardTitle>
-        <CardDescription>Signin with your email and password</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form onSubmit={form.handleSubmit(onSignin)}>
-          <FieldGroup className={'gap-4'}>
-            <Controller
-              name="email"
-              control={form.control}
-              render={({ field, fieldState }) => (
-                <Field>
-                  <FieldLabel htmlFor="email">Email</FieldLabel>
-                  <Input
-                    id="email"
-                    className={cn(
-                      buttonVariants({
-                        size: 'lg',
-                        variant: 'outline',
-                      })
-                    )}
-                    aria-invalid={fieldState.invalid}
-                    type="email"
-                    placeholder="Enter email"
-                    autoComplete="email"
-                    required
-                    {...field}
-                    name="email"
-                  />
-                  {fieldState.invalid && (
-                    <FieldError errors={[fieldState.error]} />
-                  )}
-                </Field>
-              )}
-            />
+    <div className="py-8">
+      <div className="relative flex overflow-hidden mx-auto lg:max-w-4xl">
+        <Image
+          src={
+            'https://res.cloudinary.com/dak4fznwo/image/upload/v1767237743/blog-mern/banners/man-on-a-street.jpg'
+          }
+          width={1920}
+          height={620}
+          sizes={'lg'}
+          alt="Sign In page"
+          loading="eager"
+          className="hidden md:block object-cover max-w-md rounded-tl-lg rounded-bl-lg"
+        />
 
-            <Controller
-              name="password"
-              control={form.control}
-              render={({ field, fieldState }) => (
-                <Field>
-                  <FieldLabel htmlFor="password">Password</FieldLabel>
-                  <Input
-                    id="password"
-                    className={cn(
-                      buttonVariants({
-                        size: 'lg',
-                        variant: 'outline',
-                      })
-                    )}
-                    aria-invalid={fieldState.invalid}
-                    type="password"
-                    autoComplete="current-password"
-                    placeholder="Enter password"
-                    required
-                    {...field}
-                    name="password"
-                  />
-                  {fieldState.invalid && (
-                    <FieldError errors={[fieldState.error]} />
-                  )}
-                </Field>
-              )}
-            />
-            <div className="gap-4" />
-            <Button
-              className={cn(
-                buttonVariants({
-                  variant: 'default',
-                  size: 'lg',
-                })
-              )}>
-              {isPending ?
-                <>
-                  <Loader2 className="size-4 animate-spin" />
-                  <span>Loading...</span>
-                </>
-              : <span>Sign In</span>}
-            </Button>
-          </FieldGroup>
-        </form>
-        <p className="flex justify-center w-full text-muted-foreground text-xs pt-3">
-          Don&apos;t have an account?. &nbsp;{' '}
-          <Link className="text-blue-900" href="/auth/sign-up">
-            Click here
-          </Link>
-          &nbsp; to sign up.
-        </p>
-      </CardContent>
-    </Card>
+        <div className="w-full p-0 md:p-4 lg:w-1/2">
+          <h1 className="text-3xl font-semibold text-gray-700 text-center">
+            Welcome back!
+          </h1>
+          <p className="text-sm text-gray-600 text-center mt-2 mb-0 md:mb-8">
+            It hasn&apos;t been the same without you.
+          </p>
+          <div className="flex mt-4 md:hidden" />
+
+          <div className="flex flex-col-reverse md:flex-col justify-center w-full">
+            <div>
+              <Link
+                href="#"
+                className="flex items-center justify-center mb-4 text-white rounded-lg shadow-none md:shadow-md hover:bg-gray-100">
+                <div className="px-0 py-3">
+                  <GoogleLogo />
+                </div>
+                <h2 className="px-0 md:px-4 py-3 w-4/8 text-center text-gray-600 font-bold">
+                  Sign in with Google
+                </h2>
+              </Link>
+            </div>
+
+            <div className="my-4 flex items-center justify-between">
+              <span className="border-b w-1/5 lg:w-1/4"></span>
+              <Link
+                href="#"
+                className="text-xs text-center text-gray-500 uppercase">
+                or login with email
+              </Link>
+              <span className="border-b w-1/5 lg:w-1/4"></span>
+            </div>
+
+            <Card className="ring-0 rounded-lg md:shadow-lg mb-4 md:mb-0 md:px-4 mt-4 py-8 animate-in fade-in slide-in-from-bottom-4 duration-500">
+              <CardHeader>
+                <CardTitle>Sign In</CardTitle>
+                <CardDescription>
+                  Signin with your email and password
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <form onSubmit={form.handleSubmit(onSignin)}>
+                  <FieldGroup className={'gap-4'}>
+                    <Controller
+                      name="email"
+                      control={form.control}
+                      render={({ field, fieldState }) => (
+                        <Field>
+                          <FieldLabel htmlFor="email">Email</FieldLabel>
+                          <Input
+                            id="email"
+                            className={cn(
+                              buttonVariants({
+                                size: 'lg',
+                                variant: 'outline',
+                              })
+                            )}
+                            aria-invalid={fieldState.invalid}
+                            type="email"
+                            placeholder="Enter email"
+                            autoComplete="email"
+                            required
+                            {...field}
+                            name="email"
+                          />
+                          {fieldState.invalid && (
+                            <FieldError errors={[fieldState.error]} />
+                          )}
+                        </Field>
+                      )}
+                    />
+
+                    <Controller
+                      name="password"
+                      control={form.control}
+                      render={({ field, fieldState }) => (
+                        <Field>
+                          <FieldLabel htmlFor="password">Password</FieldLabel>
+                          <Input
+                            id="password"
+                            className={cn(
+                              buttonVariants({
+                                size: 'lg',
+                                variant: 'outline',
+                              })
+                            )}
+                            aria-invalid={fieldState.invalid}
+                            type="password"
+                            autoComplete="current-password"
+                            placeholder="Enter password"
+                            required
+                            {...field}
+                            //Field has name,value, onChange
+                            //controller makes it redundant but it was here for reason - comeback
+                            name="password"
+                          />
+                          {fieldState.invalid && (
+                            <FieldError errors={[fieldState.error]} />
+                          )}
+                        </Field>
+                      )}
+                    />
+                    <div className="gap-4" />
+                    <Button
+                      type="submit"
+                      disabled={isPending}
+                      className={cn(
+                        buttonVariants({
+                          variant: 'default',
+                          size: 'lg',
+                        })
+                      )}>
+                      {isPending ?
+                        <>
+                          <Loader2 className="mr-2 size-4 animate-spin" />
+                          <span>Loading...</span>
+                        </>
+                      : <span>Sign In</span>}
+                    </Button>
+                  </FieldGroup>
+                </form>
+                <p className="flex justify-center w-full text-muted-foreground text-xs pt-3">
+                  Don&apos;t have an account? &nbsp;{' '}
+                  <Link className="text-blue-900" href="/auth/sign-up">
+                    Click here
+                  </Link>
+                  &nbsp; to sign up.
+                </p>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -16,6 +16,7 @@ import {
   Input,
 } from '@/components/ui';
 
+import Image from 'next/image';
 import { signUpSchema } from '@/app/schemas/auth';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Controller, useForm } from 'react-hook-form';
@@ -66,125 +67,173 @@ export default function SignUpPage() {
   }; //onSignup
 
   return (
-    <Card className="px-4 py-8 animate-in fade-in slide-in-from-bottom-4 duration-500">
-      <CardHeader>
-        <CardTitle>Sign Up</CardTitle>
-        <CardDescription>Create an account to get started</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form onSubmit={form.handleSubmit(onSignup)}>
-          <FieldGroup className={'gap-4'}>
-            <Controller
-              name="name"
-              control={form.control}
-              render={({ field, fieldState }) => (
-                <Field>
-                  <FieldLabel htmlFor="name">Fullname</FieldLabel>
-                  <Input
-                    id="name"
-                    className={cn(
-                      buttonVariants({
-                        size: 'lg',
-                        variant: 'outline',
-                      })
-                    )}
-                    aria-invalid={fieldState.invalid}
-                    type="name"
-                    placeholder="Enter name"
-                    autoComplete="name"
-                    required
-                    {...field}
-                    name="name"
-                  />
-                  {fieldState.invalid && (
-                    <FieldError errors={[fieldState.error]} />
-                  )}
-                </Field>
-              )}
-            />
+    <div className="py-8">
+      <div className="relative flex overflow-hidden mx-auto lg:max-w-4xl">
+        <Image
+          src={
+            'https://res.cloudinary.com/dak4fznwo/image/upload/v1767242722/next-blog/idcskllvhntoqtuqsot2.png'
+          }
+          width={1920}
+          height={620}
+          sizes={'lg'}
+          alt="Sign Up page"
+          loading="eager"
+          className="hidden md:block object-cover max-w-md rounded-tl-lg rounded-bl-lg"
+        />
 
-            <Controller
-              name="email"
-              control={form.control}
-              render={({ field, fieldState }) => (
-                <Field>
-                  <FieldLabel htmlFor="email">Email</FieldLabel>
-                  <Input
-                    id="email"
-                    className={cn(
-                      buttonVariants({
-                        size: 'lg',
-                        variant: 'outline',
-                      })
-                    )}
-                    aria-invalid={fieldState.invalid}
-                    type="email"
-                    placeholder="Enter email"
-                    autoComplete="email"
-                    required
-                    {...field}
-                    name="email"
-                  />
-                  {fieldState.invalid && (
-                    <FieldError errors={[fieldState.error]} />
-                  )}
-                </Field>
-              )}
-            />
+        <div className="w-full p-0 md:p-4 lg:w-1/2">
+          <h1 className="text-3xl font-semibold text-gray-700 text-center">
+            Because your 3am Ideas Deserve Better!
+          </h1>
 
-            <Controller
-              name="password"
-              control={form.control}
-              render={({ field, fieldState }) => (
-                <Field>
-                  <FieldLabel htmlFor="password">Password</FieldLabel>
-                  <Input
-                    id="password"
-                    className={cn(
-                      buttonVariants({
-                        size: 'lg',
-                        variant: 'outline',
-                      })
-                    )}
-                    aria-invalid={fieldState.invalid}
-                    type="password"
-                    autoComplete="current-password"
-                    placeholder="Enter password"
-                    required
-                    {...field}
-                    name="password"
-                  />
-                  {fieldState.invalid && (
-                    <FieldError errors={[fieldState.error]} />
-                  )}
-                </Field>
-              )}
-            />
-            <div className="gap-4" />
-            <Button
-              className={cn(
-                buttonVariants({
-                  variant: 'default',
-                  size: 'lg',
-                })
-              )}>
-              {isPending ?
-                <>
-                  <Loader2 className="size-4 animate-spin" />
-                  <span>Loading...</span>
-                </>
-              : <span>Sign Up</span>}
-            </Button>
-          </FieldGroup>
-        </form>
-        <p className="flex justify-center w-full text-muted-foreground text-xs pt-3">
-          Already have an account? &nbsp;{' '}
-          <Link className="text-blue-900" href="/auth/sign-in">
-            Click here
-          </Link>
-          &nbsp; to sign in.
-        </p>
-      </CardContent>
-    </Card>
+          <div className="flex mb-6 md:mb-0" />
+
+          <div className="flex flex-col justify-center w-full">
+            <div className="my-4 flex items-center justify-center">
+              <Image
+                aria-label="CackeStack UK"
+                width={30}
+                height={30}
+                src={
+                  'https://res.cloudinary.com/dak4fznwo/image/upload/v1769746264/blog-mern/icons/logos/ccake_bzspcr.svg'
+                }
+                sizes={'sm'}
+                alt="Logo"
+                loading="eager"
+                className="flex object-cover"
+              />
+              <span className="font-medium tracking-wide">akeStack.UK</span>
+            </div>
+
+            <Card className="ring-0 rounded-lg md:shadow-lg mb-4 md:mb-0 md:px-4 mt-3 py-8 animate-in fade-in slide-in-from-bottom-4 duration-500">
+              <CardHeader>
+                <CardTitle>Sign Up</CardTitle>
+                <CardDescription>
+                  Create an account to get started
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <form onSubmit={form.handleSubmit(onSignup)}>
+                  <FieldGroup className={'gap-4'}>
+                    <Controller
+                      name="name"
+                      control={form.control}
+                      render={({ field, fieldState }) => (
+                        <Field>
+                          <FieldLabel htmlFor="name">Fullname</FieldLabel>
+                          <Input
+                            id="name"
+                            className={cn(
+                              buttonVariants({
+                                size: 'lg',
+                                variant: 'outline',
+                              })
+                            )}
+                            aria-invalid={fieldState.invalid}
+                            type="text"
+                            placeholder="Enter name"
+                            autoComplete="name"
+                            required
+                            {...field}
+                            //come back not extremely important since it fixed the problem
+                            //but it is redundant, fields is within controller.
+                            name="name"
+                          />
+                          {fieldState.invalid && (
+                            <FieldError errors={[fieldState.error]} />
+                          )}
+                        </Field>
+                      )}
+                    />
+
+                    <Controller
+                      name="email"
+                      control={form.control}
+                      render={({ field, fieldState }) => (
+                        <Field>
+                          <FieldLabel htmlFor="email">Email</FieldLabel>
+                          <Input
+                            id="email"
+                            className={cn(
+                              buttonVariants({
+                                size: 'lg',
+                                variant: 'outline',
+                              })
+                            )}
+                            aria-invalid={fieldState.invalid}
+                            type="email"
+                            placeholder="Enter email"
+                            autoComplete="email"
+                            required
+                            {...field}
+                            name="email"
+                          />
+                          {fieldState.invalid && (
+                            <FieldError errors={[fieldState.error]} />
+                          )}
+                        </Field>
+                      )}
+                    />
+
+                    <Controller
+                      name="password"
+                      control={form.control}
+                      render={({ field, fieldState }) => (
+                        <Field>
+                          <FieldLabel htmlFor="password">Password</FieldLabel>
+                          <Input
+                            id="password"
+                            className={cn(
+                              buttonVariants({
+                                size: 'lg',
+                                variant: 'outline',
+                              })
+                            )}
+                            aria-invalid={fieldState.invalid}
+                            type="password"
+                            autoComplete="new-password"
+                            placeholder="Enter password"
+                            required
+                            {...field}
+                            name="password"
+                          />
+                          {fieldState.invalid && (
+                            <FieldError errors={[fieldState.error]} />
+                          )}
+                        </Field>
+                      )}
+                    />
+                    <div className="gap-4" />
+                    <Button
+                      disabled={isPending}
+                      type="submit"
+                      className={cn(
+                        buttonVariants({
+                          variant: 'default',
+                          size: 'lg',
+                        })
+                      )}>
+                      {isPending ?
+                        <>
+                          <Loader2 className="mr-2 size-4 animate-spin" />
+                          <span>Loading...</span>
+                        </>
+                      : <span>Sign Up</span>}
+                    </Button>
+                  </FieldGroup>
+                </form>
+                <p className="flex justify-center w-full text-muted-foreground text-xs pt-3">
+                  Already have an account? &nbsp;{' '}
+                  <Link className="text-blue-900" href="/auth/sign-in">
+                    Click here
+                  </Link>
+                  &nbsp; to sign in.
+                </p>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
