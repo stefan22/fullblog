b3623d7ea0a8656e13040e2116973a7752aa5d6c
Stefano L <stefanosemail@proton.me>
2026-09-06 08:09:07 +0100

    amends to cover hover, split txt & back nav all&ok

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -6,7 +6,7 @@ import { cn } from '@/lib/utils';
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-screen mb-0 w-full flex items-center justify-center">
+    <div className="relative min-h-screen mb-0 w-full flex items-center justify-center">
       <div className="absolute top-5 left-5">
         <Link href="/" className={cn(buttonVariants({ variant: 'secondary' }))}>
           <ArrowLeft className="size-4" />
